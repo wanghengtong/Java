47087249dc9e3fa6892dacc219326da4ce288c92
“wanghengtong” <wanghengtong@chiansec.com>
2025-09-29 12:00:35 +0800

    ClassFinal代码加密混淆

---
+++ ClassFinal/src/main/java/com/wanghengtong/classfinal/ClassFinalApplication.java
@@ -0,0 +1,13 @@
+package com.wanghengtong.classfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassFinalApplication.class, args);
+    }
+
+}
---
+++ ClassFinal/src/test/java/com/wanghengtong/classfinal/ClassFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanghengtong.classfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
